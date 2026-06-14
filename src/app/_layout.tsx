647aeef2b803e8c15a2e99541cf4868f71d6a724
HTinh2512 <22641641.tinh@student.iuh.edu.vn>
2026-06-14 17:57:12 +0700

    db-completed

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -92,6 +92,13 @@ export default function TabLayout() {
             ),
           }}
         />
+
+        <Tabs.Screen
+          name="index"
+          options={{
+            href: null,
+          }}
+        />
       </Tabs>
     </ThemeProvider>
   );
